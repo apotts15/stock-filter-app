46b1452f578ae35e5fb55ca2af017212b4ae8e69
Andrew Pottenger <apotts15@gmail.com>
2017-08-08 00:37:45 -0700

    fix that mess

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -29,4 +29,4 @@ var all = {
 // ==============================================
 module.exports = _.merge(
     all,
-    require('./' + process.env.NODE_ENV + '.js') || {});
+    require('./' + process.env.NODE_ENV) || {});
